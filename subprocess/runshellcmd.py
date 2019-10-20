981f236348ab617f2cd5b5806418b237a321654d
mrpangkevin <pk0206@hotmail.com>
2019-10-19 19:34:01 -0700

    update subprocess

--- subprocess/runshellcmd.py
+++ subprocess/runshellcmd.py
@@ -7,15 +7,28 @@ def runcmd(cmd):
     print("cmd:%s" % cmd)
     p1=subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     p1.wait()
+    out=p1.stdout.readlines()
+    err=p1.stderr.readlines()
     if(p1.poll()==0):
-        out=p1.stdout.readlines()
         print(out)
     else:
-        err=p1.stderr.readlines()
         print("Failed:" ,err)
+    return out,err
+
+def saveout(out, err):
+    file_log = open("log", "a")
+    file_err = open("err", "a")
+    print >> file_log, out
+    print >> file_err, err
+    file_log.close()
+    file_err.close()
 
 if __name__ == "__main__"    :
-    runcmd("ls /")
-    runcmd("ls abc")
-    runcmd("exit 1")
-    runcmd("java -version")
+    out,err = runcmd("ls /")
+    saveout(out, err)
+    out,err = runcmd("ls abc")
+    saveout(out, err)
+    out,err = runcmd("exit 1")
+    saveout(out, err)
+    out,err = runcmd("java -version")
+    saveout(out, err)
